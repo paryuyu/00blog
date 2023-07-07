2163798f794152a0a153b2f594c470b5f78415cf
paryuyu <107971288+paryuyu@users.noreply.github.com>
2023-07-07 14:43:46 +0900

    ogimg upload@

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,18 +2,25 @@ import './globals.css'
 import Nav from '@/components/Nav'
 import Provider from '@/components/Provider'
 
-
+export const openGraphImg = { images: ['/ogImg.png'] }
 export const metadata = {
   title: '00blog',
   description: 'custom blog project',
-  icons:{
-    icon:'/favicon.ico'
-  }
+  icons: {
+    icon: '/favicon.ico'
+  },
+
+  openGraph: {
+    ...openGraphImg,
+    title: '00Blog',
+    description: '카카오톡으로 간단하게 로그인하여 사용해보세요.',
+
+  },
 }
 
 
 export default function RootLayout({ children }) {
- 
+
 
   return (
     <html lang="en">
