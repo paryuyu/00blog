7b0791bc19a69d1fc246f83ed900f9c30826ffb1
paryuyu <107971288+paryuyu@users.noreply.github.com>
2023-07-19 14:39:08 +0900

    환경변수설정

--- next.config.js
+++ next.config.js
@@ -8,6 +8,7 @@ const nextConfig = {
     ],
   },
   env: {
+    BASE_URL: process.env.BASE_URL,
     CLOUD_NAME: process.env.CLOUD_NAME,
     CLOUD_API_KEY: process.env.CLOUD_API_KEY,
     CLOUD_API_SECRET: process.env.CLOUD_API_SECRET,
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,13 @@
 import Feed from '@/components/Feed';
 import "../app/globals.css";
 import { Bagel_Fat_One } from 'next/font/google';
+import { env } from '../../next.config';
 
 const Bagel = Bagel_Fat_One({ subsets: ['latin'], weight: "400" });
 
 async function getData() {
   try {
-    let response = await fetch('http://localhost:3000/api/post');
+    let response = await fetch(env.BASE_URL+'/api/post');
     let result = await response.json();
     return result;
   } catch (err) {
